5b180ec4ffd4124a44250d13591e4a203003765b
Higor <higorfequer@gmail.com>
2023-05-08 21:56:56 -0300

    08/05 JS

---
+++ Higor/Workspace/JS/Aula2/main.js
@@ -0,0 +1,11 @@
+// multiplicação
+
+// comparação
+var comparação = ovo1 < ovo2
+console.log(compara) //false
+
+// terceiro tipo de comparação
+var variavel1 = 3
+var variavel2 = 3
+var compara2 = variavel1 <= variavel2 != variavel1 > variavel2
+console.log(!compara2) //tuer
